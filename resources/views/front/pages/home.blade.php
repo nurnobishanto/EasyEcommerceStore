35337a617ac4615a20bc25154d36ef70256c0b5c
Md Nurnobi Hosen <47949519+nurnobishanto@users.noreply.github.com>
2023-09-21 15:03:37 +0600

    Update home.blade.php

--- resources/views/front/pages/home.blade.php
+++ resources/views/front/pages/home.blade.php
@@ -11,8 +11,22 @@
         <div class="container">
             <div class="row">
                 <div class="col-12 mb-6">
+                    <h3 class="mb-0 d-inline">Featured Products</h3>
+                </div>
+            </div>
+
+            <div class="row g-4 row-cols-lg-5 row-cols-2 row-cols-md-3">
+                @foreach(featuredProducts() as $product)
+                    @include('front.includes.single_product',['product' => $product])
+                @endforeach
+            </div>
+        </div>
+    </section>
+    <section class="my-lg-2 my-8">
+        <div class="container">
+            <div class="row">
+                <div class="col-12 mb-6">
                     <h3 class="mb-0 d-inline">Popular Products</h3>
-                    <a href="#" class="btn btn-sm btn-primary">Shop All</a>
                 </div>
             </div>
 
