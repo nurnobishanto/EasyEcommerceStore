d5e414e8c7350c0832553ca6b6dcae1aaaad87a7
Md Nurnobi Hosen <47949519+nurnobishanto@users.noreply.github.com>
2023-10-29 16:01:33 +0600

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,11 +19,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-//        $subscription_last_check = getSetting('subscription_last_check')??'1997-01-01';
-//        $exp_date = getSetting('subscription_expire_date')??'1997-01-01';
-//        if (($subscription_last_check < date('Y-m-d',time())) || ($exp_date < date('Y-m-d',time())) ){
-//            setSetting('subscription_last_check',date('Y-m-d',time()) );
-//            coorHelper();
-//        }
+        $subscription_last_check = getSetting('subscription_last_check')??'1997-01-01';
+        $exp_date = getSetting('subscription_expire_date')??'1997-01-01';
+        if (($subscription_last_check < date('Y-m-d',time())) || ($exp_date < date('Y-m-d',time())) ){
+            setSetting('subscription_last_check',date('Y-m-d',time()) );
+            coorHelper();
+        }
     }
 }
